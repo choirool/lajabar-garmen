0401d2e8b83c139c71269461c5a7db2ed1728d36
choirool <khoiroelf@gmail.com>
2020-10-02 07:45:09 +0800

    Initial commit

---
+++ database/migrations/2020_10_01_231058_alter_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')->unique()->after('name');
+            $table->dropUnique('users_email_unique');
+            $table->string('email')->unique()->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username');
+            $table->dropUnique('users_email_unique');
+            $table->string('email')->unique()->change();
+        });
+    }
+}
