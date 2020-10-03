456e1feedc77c55e199816974119df74e5ea8874
choirool <khoiroelf@gmail.com>
2020-10-03 13:39:29 +0800

    Remove welcome page

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,11 @@ use App\Http\Livewire\Users\UpdateUsers;
 use App\Http\Livewire\Users\Users;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    });
+
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
