31b58ecdde3ae41f85cc4e0aad3c1a25a225d0af
choirool <khoiroelf@gmail.com>
2020-11-17 17:38:48 +0800

    Filter zero amount on payment

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use App\Models\Order;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Payment extends Model
 {
@@ -20,6 +21,13 @@ class Payment extends Model
         'amount',
         'meta',
     ];
+
+    protected static function booted()
+    {
+        static::addGlobalScope('notZero', function (Builder $builder) {
+            $builder->where('amount', '>', 0);
+        });
+    }
 
     protected $casts = [
         'meta' => 'array',
